20e312d28909cbd4cdf0db2083237b44327bb76e
adrian <adrian.drozman@softvision.com>
2024-05-10 17:30:30 +0300

    new changes

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const PORT = 8094;
 const app = express();
 
 app.get('/', (req,res)=>{
-    return res.status(200).json({message: `hellgoo from ${os.hostname}`});
+    return res.status(200).json({message: `heloo from ${os.hostname}, ai de plm mere!`});
 })
 
 app.listen(PORT, ()=>console.log(`Server started at port ${PORT}`));
