b481b00181c76013a07d4895853adf3f61ce7cbe
adrian <adrian.drozman@softvision.com>
2024-05-10 17:24:50 +0300

    3

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const os = require('os');
+
+const PORT = 8094;
+const app = express();
+
+app.get('/', (req,res)=>{
+    return res.status(200).json({message: `hellgoo from ${os.hostname}`});
+})
+
+app.listen(PORT, ()=>console.log(`Server started at port ${PORT}`));
