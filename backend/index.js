5cce71a2d08fc5fe048b720f4cdf483cc83e42de
adrian <adrian.drozman@softvision.com>
2024-05-12 11:15:55 +0300

    foo2

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const PORT = 8094;
 const app = express();
 
 app.get('/', (req,res)=>{
-    return res.status(200).json({message: `heloo from ${os.hostname}, `});
+    return res.status(200).json({message: `heloo from ${os.hostname}, final test`});
 })
 
 app.listen(PORT, ()=>console.log(`Server started at port ${PORT}`));
